0fd4019e5f3df2e346b57640eb658a4c9974b1ea
ahmedtom <101574426+ahmedtom@users.noreply.github.com>
2022-06-28 23:23:24 +0200

    Update التشغيل.py

--- Musicjepthon/التشغيل.py
+++ Musicjepthon/التشغيل.py
@@ -119,7 +119,7 @@ async def ytdl(link):
         return 0, stderr.decode()
 
 
-@Client.on_message(filters.command(["تشغيل"], prefixes=f"{HNDLR}"))
+@Client.on_message(filters.command(["تشغيل", "play"], prefixes=f"{HNDLR}"))
 async def play(client, m: Message):
     replied = m.reply_to_message
     chat_id = m.chat.id
